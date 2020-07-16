4c12def4a65eda4c04468f339e782ae81d947684
Emil <darksky270990@gmail.com>
2020-07-17 00:03:39 +0300

    Lesson 1

---
+++ src/com/homework/first/FirstMission.java
@@ -0,0 +1,43 @@
+package com.homework.first;
+
+public class FirstMission implements Comparable<FirstMission> {
+    //test comparable
+    private Integer integer;
+    private Double aDouble;
+
+    public FirstMission(Integer integer, double aDouble) {
+        this.integer = integer;
+        this.aDouble = aDouble;
+    }
+
+    @Override
+    public int compareTo(FirstMission firstMission) {
+        try {
+            return compareNum(aDouble,aDouble) + compareNum(this.integer, firstMission.integer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -666;
+        }
+    }
+
+    public <T extends Number & Comparable<? super T>> int compareNum(T t1, T t2) throws Exception {
+        if (!(t1 instanceof Double && t2 instanceof Double) || !(t1 instanceof Integer && t2 instanceof Integer)) {
+            throw new Exception("Different Type: I dont want compare... ");
+        }
+
+        return t1.compareTo(t2);
+    }
+
+
+    @Override
+    public String toString() {
+        return "FirstMission{" +
+               '\'' +
+               ", Integer=" + integer +
+               ", double =" + aDouble +
+               '\'' +
+               '}';
+    }
+
+
+}
---
+++ src/com/homework/first/Mathable.java
@@ -0,0 +1,10 @@
+package com.homework.first;
+
+public interface Mathable<T> {
+
+    T sum(T t1, T t2);
+    T div(T t1, T t2);
+    T mul(T t1, T t2);
+    T sub(T t1, T t2);
+
+}
